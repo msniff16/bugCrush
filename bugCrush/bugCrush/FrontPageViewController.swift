a982d40981ea747b71bc9208b1884ddcdee34a2c
Matthew <matthew.sniff@climate.com>
2015-10-14 09:36:09 -0700

    added cocoapods and spinner

--- bugCrush/bugCrush/FrontPageViewController.swift
+++ bugCrush/bugCrush/FrontPageViewController.swift
@@ -10,16 +10,10 @@ import UIKit
 
 class FrontPageViewController: UIViewController {
 
+    // view loaded
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
---
+++ bugCrush/bugCrush/PickFarmerViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PickFarmerViewController.swift
+//  bugCrush
+//
+//  Created by Matthew Sniff on 10/14/15.
+//  Copyright © 2015 Marlon Misra. All rights reserved.
+//
+
+import UIKit
+import SwiftSpinner
+// use spinner here: https://github.com/icanzilb/SwiftSpinner
+
+class PickFarmerViewController: UIViewController {
+
+    // view loaded
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+
+}
