bb7b1d668c286c9f44451cc0ad2ef6fd2b6884b6
cnorgate <cnorgate@tcc-cnorgate2.local>
2015-10-20 11:35:11 -0700

    Got the controls for the target working

--- bugCrush/bugCrush/GameScreenViewController.swift
+++ bugCrush/bugCrush/GameScreenViewController.swift
@@ -13,12 +13,57 @@ class GameScreenViewController: UIViewController {
     var farmerName: String?
     @IBOutlet weak var farmerChosen: UILabel!
     
+    @IBOutlet weak var target: UIImageView!
+    var targetOrigin: CGPoint!
+    
+    var targetMoveAmount: CGFloat!
+    
+    
+    
     // view loaded
     override func viewDidLoad() {
         super.viewDidLoad()
         farmerChosen.text = farmerName
+        
+        targetOrigin = target.center
+        targetMoveAmount = 20
 
     }
+    
+    
+    
+    
+    
+    
+    // MANAGEMENT OF THE ARROWS AND TARGET
+    
+    @IBAction func upArrow(sender: AnyObject) {
+        print("up")
+        //target.center = CGPoint(x: target.center.x, y: target.center.y - 20)
+        target.center.y -= targetMoveAmount
+    }
+
+    @IBAction func rightArrow(sender: AnyObject) {
+        print("right")
+        target.center.x += targetMoveAmount
+    }
+    
+    @IBAction func downArrow(sender: AnyObject) {
+        target.center.y += targetMoveAmount
+        print("down")
+    }
+    
+    @IBAction func leftArrow(sender: AnyObject) {
+        target.center.x -= targetMoveAmount
+        print("left")
+    }
+    
+    
+    
+    
+    
+    
+    
     
     
     
